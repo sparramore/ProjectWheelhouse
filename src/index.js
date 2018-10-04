450ee110c95d252984c1471d9139e394698acf66
Sparramore <steven.parramore@gmail.com>
2018-10-04 18:46:44 -0400

    failed to remove all of the mongoose stuff from the react components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-var mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost/week18Populater", { useNewUrlParser: true });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
